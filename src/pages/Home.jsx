48d50f5bd4affa6ce3be95a9d52cf260a75d7254
Sakib <sakib.hstu.bd@gmail.com>
2023-07-28 12:42:11 +0600

    navbar and banner section done

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './Navbar';
+import Banner from './Banner';
+
+const Home = () => {
+    return (
+        <>
+            <Navbar></Navbar>
+            <Banner></Banner>
+        </>
+    );
+};
+
+export default Home;
